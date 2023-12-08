7c02092e952e999c15d64a00d0ed1428b8ed731d
weijiale <weijiale@payeasenet.com>
2023-12-08 14:50:29 +0800

    fix: 修复首页显示错误

--- src/dev-middleware.ts
+++ src/dev-middleware.ts
@@ -34,7 +34,12 @@ export const devServerMiddleware = (
     url.startsWith('/') && (url = url.substring(1));
     url.endsWith('/index.html') && (url = url.substring(0, url.length - 11));
     url.endsWith('/') && (url = url.substring(0, url.length - 1));
+    // 如果是首页则指定为当前目录
+    if(url === ''){
+      url = '.'
+    }
     for (let index = 0; index < entries.length; index++) {
+      // 如果对应虚拟模板存在
       if (entries[index].virtualTemplateFileRelativePath === url) {
         const templateContent = fs.readFileSync(
           path.resolve(entries[index].templatePath, `${opt.templateName}.html`),
